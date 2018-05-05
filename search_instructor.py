bd74f27d6216fcdbc7f55eaee8421db0c63a2b98
zzzzz0 <joyee.zyy@hotmail.com>
2018-05-05 12:35:39 -0400

    add comment 2

--- search_instructor.py
+++ search_instructor.py
@@ -1,18 +1,19 @@
 import pandas as pd
-from tabulate import tabulate
 
 
 def search_instr(instructor_df, name):
+    """
+    Arguments:
+        instructor_df: {DataFrame} -- a DataFrame of all instructor's info.
+        name: {String} -- a keyword of instructor's name
+
+    Returns:
+        DataFrame - a DataFrame contains the relevant information of the relevant instructor name
+    """
     try:
         instructor_df = pd.DataFrame(instructor_df)
-
         target = instructor_df[instructor_df['teacher_name'].str.contains(name, case=False)]
         target = target.sort_values(by='score', ascending=False)
-
         return target
-
-        # print(tabulate([['95880', 'Python for Developer', 'Kolowitz, Brian'],
-        #                 ['95888', 'Data Focused Python', 'Kolowitz, Brian'],
-        #                 ['33331', 'Physical Mechanics I', 'Quinn, Brian']], headers=['Code', 'Name', 'Instructor'], tablefmt='orgtbl'))
     except Exception:
         search_instr(instructor_df)
